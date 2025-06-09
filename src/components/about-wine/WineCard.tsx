e1c0777a636f00986e6f00879bddf2021c9bb88b
seong5 <greenbi0852@gmail.com>
2025-06-09 15:52:53 +0900

    ✨ [Feature/21] 와인카드 제작 (#29)
    * 🔧Chore: eslint.config.mjs 파일 규칙 추가 Resolve: #21
    
    * ✨Feat:폴더명 변경
    
    컨벤션에 맞게 폴더명을 변경했습니다
    
    Resolve: #21
    
    * ✨Feat:폴더경로변경 /  WineCard 타입변경
    
    코드리뷰 수정사항들 변경했습니다
    
    Resolve: #21
    
    ---------
    
    Co-authored-by: Habist <31752426+Habist@users.noreply.github.com>

---
+++ src/components/about-wine/WineCard.tsx
@@ -0,0 +1,43 @@
+type WineCardProps = {
+  data: {
+    id: number;
+    name: string;
+    region: string;
+    image: string;
+    price: number;
+    type: string;
+  };
+};
+
+const WineCard: React.FC<WineCardProps> = ({ data }) => {
+  return (
+    <section className="border border-gray-300 rounded-[16px] w-[1140px] h-[260px] flex px-8 gap-10">
+      <div className="w-[200px] h-full bg-white rounded-md flex items-end justify-center text-gray-400 text-sm">
+        {data.image ? (
+          <img
+            alt={data.name}
+            className="w-[200px] h-[209px] object-contain object-bottom"
+            src={data.image}
+          />
+        ) : (
+          "이미지"
+        )}
+      </div>
+
+      <div className="flex flex-col justify-between h-[209px] py-2 mt-6">
+        <div>
+          <div className="font-[600] text-[30px] leading-tight break-words max-w-[600px]">
+            {data.name}
+          </div>
+          <div className="font-[400] text-[16px] text-gray-500 mt-10">{data.region}</div>
+        </div>
+        <div className="bg-light-purple rounded-[12px] w-[114px] h-[37px] flex items-center justify-center mt-auto">
+          <span className="text-purple font-[700] text-[18px]">
+            ₩ {data.price.toLocaleString()}
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default WineCard;
