b667d158d7155cff960bc94bce2733bf52184b99
two678 <two6788@gmail.com>
2025-06-11 14:27:10 +0900

    ✨ Feat: 공통 레이아웃 (헤더 푸터) 추가
    Resolves: #37

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import "./globals.css";
 
 import localFont from "next/font/local";
+import { usePathname } from "next/navigation";
 
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
 import Modal from "@/components/modal/Modal";
 import { cn } from "@/utils/cn";
 
@@ -17,14 +22,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const marginClass = pathname === "/" ? "mt-6" : "mt-10";
   return (
     <html className={`${pretendard.variable}`} lang="ko">
       <body
         className={cn(pretendard.className, "flex flex-col justify-center items-center bg-white")}
       >
-        {children}
-        <div id="modal-root" />
-        <Modal />
+        <div className={cn("w-full max-w-[71.25rem] mx-auto px-5 max-mb:px-4", marginClass)}>
+          <Header />
+          <main>{children}</main>
+          <div id="modal-root" />
+          <Modal />
+        </div>
+        <Footer />
       </body>
     </html>
   );
