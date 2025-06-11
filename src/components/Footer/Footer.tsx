8ac1e024964d3a24dc6368c4411ff42eeff0a0e0
Habist <liasn@naver.com>
2025-06-11 10:31:02 +0900

    ✨ [Feature/32] 리뷰 반영

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/utils/cn";
 
 const Footer: React.FC = () => {
   return (
-    <div
+    <footer
       className={cn(
         "bg-black w-full h-[7rem] flex items-center text-white px-[2.8rem] py-[0.6rem]",
         "max-mb:h-[6rem] max-mb:px-[1.25rem] max-mb:py-[0.9375rem]"
@@ -11,12 +11,17 @@ const Footer: React.FC = () => {
       <div className="w-full flex justify-between text-sm font-normal">
         <span>@codeit - 2025</span>
         <span>15기 5팀 WHYNE</span>
-        <a className="flex gap-[0.375rem]" href="https://github.com/codeit-part3-team5/whyne">
+        <a
+          className="flex gap-[0.375rem]"
+          href="https://github.com/codeit-part3-team5/whyne"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           <img alt="github-icon" className="size-[1.25rem]" src="/images/github.png" />
           <span>GitHub</span>
         </a>
       </div>
-    </div>
+    </footer>
   );
 };
 
