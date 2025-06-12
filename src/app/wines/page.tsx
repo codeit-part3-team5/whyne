fff949f0212ef05aa28a69500f79698317c1ce74
seong5 <greenbi0852@gmail.com>
2025-06-12 18:15:10 +0900

    WineCard.tsx 반응형 적용

--- src/app/wines/page.tsx
+++ src/app/wines/page.tsx
@@ -1,15 +1,22 @@
 // 와인 목록 페이지
 import MonthlyWines from "@/components/about-wine/MonthlyWines";
+import WineCard from "@/components/about-wine/WineCard";
 import WineTypeDropDown from "@/components/about-wine/WineTypeDropDown";
 import DropDown from "@/components/DropDown";
+import winesData from "@/mocks/winesData.json";
 
-export default function winesPage() {
+export default function WinesPage() {
+  const wines = winesData.list;
+
   return (
-    <main className="flex flex-col items-center gap-6 py-10">
+    <main>
       <DropDown firstText="수정하기" secondText="삭제하기" size="small" />
       <DropDown firstText="마이페이지" secondText="로그아웃" />
       <WineTypeDropDown />
       <MonthlyWines />
+      {wines.map((wine) => (
+        <WineCard key={wine.id} data={wine} />
+      ))}
     </main>
   );
 }
--- src/components/about-wine/WineCard.tsx
+++ src/components/about-wine/WineCard.tsx
@@ -11,8 +11,8 @@ type WineCardProps = {
 
 const WineCard: React.FC<WineCardProps> = ({ data }) => {
   return (
-    <section className="border border-gray-300 rounded-[1rem] w-[71.25rem] h-[16.25rem] flex px-[0.5rem] gap-[0.625rem]">
-      <div className="w-[12.5rem] h-full bg-white rounded-md flex items-end justify-center text-gray-400 text-sm">
+    <section className="border border-gray-300 rounded-[1rem] w-full max-w-[71.25rem] h-auto flex px-[0.5rem] gap-[0.625rem]">
+      <div className="w-[12.5rem] h-full bg-white rounded-md overflow-hidden flex items-end justify-center text-gray-400 text-sm">
         {data.image ? (
           <img
             alt={data.name}
@@ -24,14 +24,14 @@ const WineCard: React.FC<WineCardProps> = ({ data }) => {
         )}
       </div>
 
-      <div className="flex flex-col justify-between h-[13.0625rem] py-[0.5rem] mt-[1.5rem]">
+      <div className="flex flex-col justify-between h-[13.0625rem] py-[0.5rem] mt-[1rem]">
         <div className="min-h-[6.25rem]">
-          <div className="font-[600] text-[1.875rem] leading-tight break-words max-w-[28.125rem] h-full">
+          <div className="font-[600] text-[1.875rem] max-[640px]:text-[1.3rem] leading-tight break-words  max-w-[28.125rem] h-full">
             {data.name}
           </div>
           <div className="font-[400] text-[1rem] text-gray-500"> {data.region}</div>
         </div>
-        <div className="bg-light-purple rounded-[0.75rem] w-[7.125rem] h-[2.3125rem] flex items-center justify-center mt-auto">
+        <div className="bg-light-purple rounded-[0.75rem] w-[7.125rem] h-[2.3125rem] flex items-center justify-center mb-5">
           <span className="text-purple font-[700] text-[1.125rem]">
             ₩ {data.price.toLocaleString()}
           </span>
