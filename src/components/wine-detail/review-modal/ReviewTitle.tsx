402b43cd3b1f2b0359e5faee5551e61859ca4d97
two678 <two6788@gmail.com>
2025-06-16 17:06:30 +0900

    ✨ Feat: 별점입력 컴포넌트 구현
    Resolves: #48

--- src/components/wine-detail/review-modal/ReviewTitle.tsx
+++ src/components/wine-detail/review-modal/ReviewTitle.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 import WineIcon from "@/assets/wine-icon.svg";
+import RatingStars from "@/components/RatingStars";
 
 interface ReviewTitleProps {
   wineName: string;
@@ -6,14 +9,23 @@ interface ReviewTitleProps {
 
 export default function ReviewTitle({ wineName }: ReviewTitleProps) {
   const size = 54;
+  const [rating, setRating] = useState(0);
 
   return (
-    <section className="flex items-center gap-4">
+    <section className="flex items-center gap-4 w-full">
       <div className="flex items-center w-17 h-full min-h-17 p-[7px] rounded-lg bg-gray100">
         <WineIcon height={size} width={size} />
       </div>
-      <div>
+      <div className="flex flex-col items-start w-[18.75rem] gap-2 max-mb:w-[11.9375rem] ">
         <h2 className="text-lg font-semibold text-gray800 leading-6.5">{wineName}</h2>
+        <div className="flex justify-between items-center gap-4">
+          <RatingStars allowHalfStar initialRating={rating} onChange={setRating} />
+          {rating > 0 && (
+            <span className="text-base max-mb:text-sm  font-medium text-center text-gray500">
+              {rating.toFixed(1)} 점
+            </span>
+          )}
+        </div>
       </div>
     </section>
   );
