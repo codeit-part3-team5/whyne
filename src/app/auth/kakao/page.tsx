572a56f1758ef6a0fc4d9a8a9e3a92fd5974e980
Habist <liasn@naver.com>
2025-06-16 16:17:20 +0900

    🐛 [Fix/61] useSearchParams 함수 빌드 에러 수정

--- src/app/auth/kakao/page.tsx
+++ src/app/auth/kakao/page.tsx
@@ -1,32 +1,13 @@
-"use client";
+import { Suspense } from "react";
 
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import KakaoRedirectPage from "./KakaoRedirectPage";
 
-import { signInWithProvider } from "@/apis/authApi";
-import Spinner from "@/components/Spinner";
-
-const KakaoRedirectPage = () => {
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const callApi = async () => {
-      if (searchParams.has("code") && searchParams.get("code") !== null) {
-        const code = searchParams.get("code")!;
-        const redirectUri = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/kakao`;
-        const res = await signInWithProvider("KAKAO", code, redirectUri);
-        localStorage.setItem("accessToken", res.accessToken);
-        localStorage.setItem("refreshToken", res.refreshToken);
-      }
-    };
-    callApi();
-  }, []);
-
+const KakaoAuthPage = () => {
   return (
-    <div className="flex justify-center">
-      <Spinner size={80} />
-    </div>
+    <Suspense>
+      <KakaoRedirectPage />
+    </Suspense>
   );
 };
 
-export default KakaoRedirectPage;
+export default KakaoAuthPage;
