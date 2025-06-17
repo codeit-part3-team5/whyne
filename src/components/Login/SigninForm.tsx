2c10a78adab9d4a8b8f74e6809a7daa4d12fc194
Habist <liasn@naver.com>
2025-06-17 14:57:40 +0900

    ✨ [Feature/67] 로그인 페이지 UI 구현

---
+++ src/components/Login/SigninForm.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import { signIn } from "@/apis/authApi";
+
+import Button from "../Button";
+import Input from "../input/Input";
+
+interface LoginFormInput {
+  email: string;
+  password: string;
+}
+const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const SigninForm = () => {
+  const {
+    register,
+    formState: { errors, isSubmitting },
+    handleSubmit,
+  } = useForm<LoginFormInput>({ mode: "all" });
+
+  const onSubmit: SubmitHandler<LoginFormInput> = async (data) => {
+    const res = await signIn(data.email, data.password);
+    localStorage.setItem("accessToken", res.accessToken);
+    localStorage.setItem("refreshToken", res.refreshToken);
+  };
+
+  return (
+    <div className="w-full mt-[4rem]">
+      <form className="flex flex-col gap-[1.5rem]" onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          {...register("email", {
+            required: "이메일은 필수 입력입니다.",
+            pattern: {
+              value: EMAIL_REGEX,
+              message: "이메일 형식으로 작성해 주세요.",
+            },
+          })}
+          aria-invalid={errors.email ? "true" : "false"}
+          error={errors.email?.message}
+          label="이메일"
+        />
+        <Input
+          {...register("password", {
+            required: "비밀번호는 필수 입력입니다.",
+          })}
+          aria-invalid={errors.password ? "true" : "false"}
+          error={errors.password?.message}
+          label="비밀번호"
+          type="password"
+        />
+        <Button
+          className="w-full mt-[2rem]"
+          disabled={isSubmitting}
+          loading={isSubmitting}
+          type="submit"
+          variant="primary"
+        >
+          로그인
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default SigninForm;
