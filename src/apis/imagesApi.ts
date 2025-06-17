38224621546473ed06f078705551c4524f8262be
hizdev <gpfksdlrn@naver.com>
2025-06-17 21:28:48 +0900

    ✨ Feat: 이미지 업로드 및 프로필 정보 수정 API 추가

---
+++ src/apis/imagesApi.ts
@@ -0,0 +1,21 @@
+import { generateSafeFileName } from "@/utils/fileUtil";
+
+import { axiosAuthClient } from "./axios/axiosConfig";
+
+export const uploadImage = async (file: File) => {
+  try {
+    const formData = new FormData();
+
+    // 파일명을 안전한 형태로 변경
+    const safeFile = generateSafeFileName(file);
+    formData.append("image", safeFile);
+    const response = await axiosAuthClient.post("/images/upload", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+
+    return response.data.url;
+  } catch (error) {
+    console.error("이미지 업로드 실패: ", error);
+    throw error;
+  }
+};
