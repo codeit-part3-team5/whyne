eafc2a720b4d09c5795f8de24ff7d695da507cf2
seong5 <greenbi0852@gmail.com>
2025-06-18 13:04:00 +0900

    Feat:post api 코드 작성

--- src/apis/winesApi.ts
+++ src/apis/winesApi.ts
@@ -1,9 +1,17 @@
 import type { BaseWineData } from "@/types/Wine";
 
-import { axiosClient } from "./axios/axiosConfig";
+import { axiosAuthClient, axiosClient } from "./axios/axiosConfig";
 
 // 와인 목록 페이지
 export const getWines = async (limit = 10): Promise<BaseWineData[]> => {
   const response = await axiosClient.get(`/wines?limit=${limit}`);
   return response.data.list;
+};
+
+//와인 등록하기 모달
+export const postWines = async (
+  newWine: Omit<BaseWineData, "id" | "avgRating" | "reviewCount" | "recentReview">
+) => {
+  const response = await axiosAuthClient.post("/wines", newWine);
+  return response.data;
 };
