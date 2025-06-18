5b642ec75df7ffcb0eaec96a56131ebda426175a
seong5 <greenbi0852@gmail.com>
2025-06-19 02:24:48 +0900

    와인 등록 api 코드 수정
    Resolve: #74

--- src/components/modal/WinePostModal.tsx
+++ src/components/modal/WinePostModal.tsx
@@ -15,12 +15,11 @@ export default function WinePostModal() {
   const { close } = useModalStore();
 
   const [name, setName] = useState("");
-  const [price, setPrice] = useState<number>();
+  const [price, setPrice] = useState("");
   const [region, setRegion] = useState("");
   const [type, setType] = useState<"Red" | "White" | "Sparkling">("Red");
   const [imageFile, setImageFile] = useState<File | null>(null);
   const [imagePreview, setImagePreview] = useState<string | null>(null);
-
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
@@ -72,7 +71,7 @@ export default function WinePostModal() {
         placeholder="가격은 숫자만 입력이 가능합니다."
         type="text"
         value={price}
-        onChange={(e) => setPrice(Number(e.target.value))}
+        onChange={(e) => setPrice(e.target.value)}
       />
 
       <div className="font-[500] text-[1rem] py-[0.425rem]">원산지</div>
@@ -88,11 +87,9 @@ export default function WinePostModal() {
       <WineTypeDropDown selected={type} onChange={setType} />
 
       <div className="font-[500] text-[1rem] py-[0.425rem] mt-4">와인 이미지</div>
-
-      {/* 숨겨진 input */}
       <input
         accept="image/*"
-        className="hidden"
+        className="absolute opacity-0 w-0 h-0"
         id="image-upload"
         type="file"
         onChange={handleImageChange}
@@ -100,10 +97,14 @@ export default function WinePostModal() {
 
       {/* 이미지 클릭 시 input 활성화 */}
       <div className="w-[140px] h-[140px] mb-4 rounded-md border-gray-300">
-        {imagePreview ? (
-          <img alt="미리보기" className="w-full h-full object-cover" src={imagePreview} />
-        ) : (
-          <label className="cursor-pointer block w-full h-full" htmlFor="image-upload">
+        <label className="cursor-pointer block w-full h-full" htmlFor="image-upload">
+          {imagePreview ? (
+            <img
+              alt="미리보기"
+              className="w-full h-full object-cover pointer-events-none"
+              src={imagePreview}
+            />
+          ) : (
             <Image
               alt="이미지 추가"
               className="object-contain"
@@ -111,18 +112,9 @@ export default function WinePostModal() {
               src={addIcon}
               width={140}
             />
-          </label>
-        )}
+          )}
+        </label>
       </div>
-
-      <input
-        accept="image/*"
-        className="hidden"
-        id="image-upload"
-        type="file"
-        onChange={handleImageChange}
-      />
-
       <div className="flex flex-row gap-4 mt-3">
         <Button
           className="w-[18.375rem] rounded-[0.75rem]"
