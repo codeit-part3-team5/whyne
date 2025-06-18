c3454b44cb6a72029b7df8f9481c48c3ab593483
hizdev <gpfksdlrn@naver.com>
2025-06-19 02:35:27 +0900

    ✨ Feat: 내가 만든 와인 목록 조회 api 추가

--- src/apis/usersApi.ts
+++ src/apis/usersApi.ts
@@ -1,4 +1,6 @@
+import { MyReviewsResponse } from "@/types/ReviewList";
 import { User } from "@/types/User";
+import { WinesResponse } from "@/types/Wine";
 
 import { axiosAuthClient } from "./axios/axiosConfig";
 
@@ -29,15 +31,33 @@ export const updateUserProfile = async (
 export const getMyReviews = async (limit: number, cursor?: number | null) => {
   try {
     const params: { limit: number; cursor?: number } = { limit };
-    if (cursor !== null) params.cursor = cursor;
+    if (cursor) params.cursor = cursor;
 
-    const response = await axiosAuthClient.get("/users/me/reviews", { params });
+    const response = await axiosAuthClient.get<MyReviewsResponse>("/users/me/reviews", { params });
     return response.data;
   } catch (error) {
     if (error instanceof Error) {
       console.error("내가 작성한 리뷰 조회 실패: ", error);
     } else {
       console.error("내가 작성한 리뷰 조회 실패: 알 수 없는 오류");
+    }
+    throw error;
+  }
+};
+
+// 내가 만든 와인 목록 조회
+export const getMyWines = async (limit: number, cursor?: number | null) => {
+  try {
+    const params: { limit: number; cursor?: number } = { limit };
+    if (cursor) params.cursor = cursor;
+
+    const response = await axiosAuthClient.get<WinesResponse>("/users/me/wines", { params });
+    return response.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("내가 만든 와인 목록 조회 실패:", error);
+    } else {
+      console.error("내가 만든 와인 목록 조회 실패: 알 수 없는 오류");
     }
     throw error;
   }
