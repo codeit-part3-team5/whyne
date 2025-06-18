f81149206bbdca94c616b60cf6e5310a4b83b8f6
hizdev <70252430+gpfksdlrn@users.noreply.github.com>
2025-06-18 11:05:24 +0900

    ✨ [Feature/69] 프로필 이미지, 프로필 정보 api 구현 (#75)
    * ✨ Feat: 이미지 업로드 및 프로필 정보 수정 API 추가
    
    * ✨ Feat: useProfile 커스텀 훅 추가
    
    * ✨ Feat: 프로필 이미지 및 닉네임 수정 기능 추가
    
    * ⚡️ Fix: 코드레빗 리뷰 적용
    
    * ⚡️ Fix: 코드레빗 리뷰 적용
    
    * ⚡️ Feat: uploadImage 함수에 제네릭 타입 추가

---
+++ src/apis/imagesApi.ts
@@ -0,0 +1,23 @@
+import { generateSafeFileName } from "@/utils/fileUtil";
+
+import { axiosAuthClient } from "./axios/axiosConfig";
+
+interface uploadImageResponse {
+  url: string;
+}
+
+export const uploadImage = async (file: File) => {
+  try {
+    const formData = new FormData();
+
+    // 파일명을 안전한 형태로 변경
+    const safeFile = generateSafeFileName(file);
+    formData.append("image", safeFile);
+    const response = await axiosAuthClient.post<uploadImageResponse>("/images/upload", formData);
+
+    return response.data.url;
+  } catch (error) {
+    console.error("이미지 업로드 실패: ", error);
+    throw error;
+  }
+};
--- src/apis/usersApi.ts
+++ src/apis/usersApi.ts
@@ -1,6 +1,26 @@
+import { User } from "@/types/User";
+
 import { axiosAuthClient } from "./axios/axiosConfig";
 
+// 내 정보 조회
 export const me: MeApi = async () => {
   const res = await axiosAuthClient.get<MeApiResponse>("/users/me");
   return res.data;
+};
+
+// 내 정보 수정 (닉네임 및 프로필 이미지)
+export const updateUserProfile = async (
+  nickname: string,
+  imageUrl: string | null
+): Promise<User> => {
+  try {
+    const response = await axiosAuthClient.patch<User>("/users/me", {
+      nickname,
+      image: imageUrl,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("내 정보 수정 실패:", error);
+    throw error;
+  }
 };
